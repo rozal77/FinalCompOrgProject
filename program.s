67e03f0d72fddd584b64688b7eaa07e67b6c7053
rozal77 <uniquerojal@gmail.com>
2023-12-04 21:46:08 -0500

    First Commit

---
+++ program.s
@@ -0,0 +1,12 @@
+    # Computer Organization Project
+    # Rojal Sapkota
+    # Fall 2023
+    # Email: rojal.sapkota@bison.howard.edu
+    # Student ID : @03086974
+
+.data   
+input_message:      .asciiz "Enter an integer: "
+illegal_message:    .asciiz "Illegal Number!\n"
+newline:            .asciiz "\n"
+
+.text   
