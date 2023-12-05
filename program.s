ccccff5b67c0d7b1a68cd16716748659ffb9ead5
rozal77 <uniquerojal@gmail.com>
2023-12-04 21:48:44 -0500

    Second Commit

--- program.s
+++ program.s
@@ -10,3 +10,11 @@ illegal_message:    .asciiz "Illegal Number!\n"
 newline:            .asciiz "\n"
 
 .text   
+main:               
+    li      $v0,    4
+    la      $a0,    input_message
+    syscall                                             # displays the input_message on the screen
+
+    li      $v0,    5                                   # takes the input from the user
+    syscall 
+    move    $t0,    $v0                                 # Moves value from $v0 to $t0
